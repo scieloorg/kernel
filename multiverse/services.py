18bfb14d3d4338b5c440063dbdbca56b03079d74
Gustavo Fonseca <gustavo@gfonseca.net>
2018-08-24 16:50:42 -0300

    Generaliza o registro de artigos

--- multiverse/services.py
+++ multiverse/services.py
@@ -11,7 +11,34 @@ class CommandHandler:
         self.Session = Session
 
 
-class RegisterArticle(CommandHandler):
+class BaseRegisterArticle(CommandHandler):
+    """Implementação abstrata de comando para registrar um novo artigo.
+
+    :param id: Identificador alfanumérico para o artigo. Deve ser único.
+    :param data_url: URL válida e publicamente acessível para o artigo em XML 
+    SciELO PS.
+    """
+
+    def _get_article(self, session: Session, id: str) -> Article:
+        raise NotImplementedError()
+
+    def _persist(self, session: Session, article: Article) -> None:
+        raise NotImplementedError()
+
+    def __call__(self, id: str, data_url: str, assets: Dict[str, str] = None) -> None:
+        try:
+            assets = dict(assets)
+        except TypeError:
+            assets = {}
+        session = self.Session()
+        article = self._get_article(session, id)
+        article.new_version(data_url)
+        for asset_id, asset_url in assets.items():
+            article.new_asset_version(asset_id, asset_url)
+        self._persist(session, article)
+
+
+class RegisterArticle(BaseRegisterArticle):
     """Registra um novo artigo.
 
     :param id: Identificador alfanumérico para o artigo. Deve ser único.
@@ -19,20 +46,14 @@ class RegisterArticle(CommandHandler):
     SciELO PS.
     """
 
-    def __call__(self, id: str, data_url: str, assets: Dict[str, str] = None) -> None:
-        try:
-            assets = dict(assets)
-        except TypeError:
-            assets = {}
-        session = self.Session()
-        article = Article(doc_id=id)
-        article.new_version(data_url)
-        for asset_id, asset_url in assets.items():
-            article.new_asset_version(asset_id, asset_url)
-        session.articles.add(article)
+    def _get_article(self, session, id):
+        return Article(doc_id=id)
+
+    def _persist(self, session, article):
+        return session.articles.add(article)
 
 
-class RegisterArticleVersion(CommandHandler):
+class RegisterArticleVersion(BaseRegisterArticle):
     """Registra uma nova versão de um artigo já registrado.
 
     :param id: Identificador alfanumérico para o artigo.
@@ -40,17 +61,11 @@ class RegisterArticleVersion(CommandHandler):
     SciELO PS.
     """
 
-    def __call__(self, id: str, data_url: str, assets: Dict[str, str] = None) -> None:
-        try:
-            assets = dict(assets)
-        except TypeError:
-            assets = {}
-        session = self.Session()
-        article = session.articles.fetch(id)
-        article.new_version(data_url)
-        for asset_id, asset_url in assets.items():
-            article.new_asset_version(asset_id, asset_url)
-        session.articles.update(article)
+    def _get_article(self, session, id):
+        return session.articles.fetch(id)
+
+    def _persist(self, session, article):
+        return session.articles.update(article)
 
 
 class FetchArticleData(CommandHandler):
