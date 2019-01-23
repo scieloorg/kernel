7f6e8ea5c4e01a48b1c097e64ae81eb28341db55
Patricia Morimoto <patricia.morimoto@scielo.org>
2019-01-23 12:13:37 -0200

    [WIP] Refatora testes de Adapters de tipo Store

--- tests/test_adapters.py
+++ tests/test_adapters.py
@@ -4,7 +4,7 @@ from unittest.mock import Mock
 from documentstore import adapters, domain, exceptions
 
 
-class DocumentsBundleStoreTest(unittest.TestCase):
+class StoreTestMixin:
     def setUp(self):
         self.DBCollectionMock = Mock()
         self.DBCollectionMock.insert_one = Mock()
@@ -12,20 +12,18 @@ class DocumentsBundleStoreTest(unittest.TestCase):
         self.DBCollectionMock.replace_one = Mock()
 
     def test_add(self):
-        bundles = adapters.DocumentsBundleStore(self.DBCollectionMock)
-        bundle = domain.DocumentsBundle(id="0034-8910-rsp-48-2")
-        bundles.add(bundle)
-        expected = bundle.manifest
+        store = self.Adapter(self.DBCollectionMock)
+        data = self.DomainClass(id="0034-8910-rsp-48-2")
+        store.add(data)
+        expected = data.manifest
         expected["_id"] = "0034-8910-rsp-48-2"
         self.DBCollectionMock.insert_one.assert_called_once_with(expected)
 
-    def test_add_bundle_with_divergent_ids(self):
-        bundles = adapters.DocumentsBundleStore(self.DBCollectionMock)
-        bundle = domain.DocumentsBundle(
-            manifest={"_id": "1", "id": "0034-8910-rsp-48-2"}
-        )
-        bundles.add(bundle)
-        expected = bundle.manifest
+    def test_add_data_with_divergent_ids(self):
+        store = self.Adapter(self.DBCollectionMock)
+        data = self.DomainClass(manifest={"_id": "1", "id": "0034-8910-rsp-48-2"})
+        store.add(data)
+        expected = data.manifest
         self.DBCollectionMock.insert_one.assert_called_once_with(expected)
 
     def test_add_raises_exception_if_already_exists(self):
@@ -34,37 +32,37 @@ class DocumentsBundleStoreTest(unittest.TestCase):
         self.DBCollectionMock.insert_one.side_effect = pymongo.errors.DuplicateKeyError(
             ""
         )
-        bundles = adapters.DocumentsBundleStore(self.DBCollectionMock)
-        bundle = domain.DocumentsBundle(id="0034-8910-rsp-48-2")
-        self.assertRaises(exceptions.AlreadyExists, bundles.add, bundle)
+        store = self.Adapter(self.DBCollectionMock)
+        data = self.DomainClass(id="0034-8910-rsp-48-2")
+        self.assertRaises(exceptions.AlreadyExists, store.add, data)
 
     def test_fetch_raises_exception_if_does_not_exist(self):
         self.DBCollectionMock.find_one.return_value = None
-        bundles = adapters.DocumentsBundleStore(self.DBCollectionMock)
-        self.assertRaises(exceptions.DoesNotExist, bundles.fetch, "0034-8910-rsp-48-2")
+        store = self.Adapter(self.DBCollectionMock)
+        self.assertRaises(exceptions.DoesNotExist, store.fetch, "0034-8910-rsp-48-2")
 
     def test_fetch(self):
         self.DBCollectionMock.find_one.return_value = {"_id": "0034-8910-rsp-48-2"}
-        bundles = adapters.DocumentsBundleStore(self.DBCollectionMock)
-        bundles.fetch("0034-8910-rsp-48-2")
+        store = self.Adapter(self.DBCollectionMock)
+        store.fetch("0034-8910-rsp-48-2")
         self.DBCollectionMock.find_one.assert_called_once_with(
             {"_id": "0034-8910-rsp-48-2"}
         )
 
-    def test_fetch_returns_documents_bundle(self):
+    def test_fetch_returns_domain_instance(self):
         manifest = {"_id": "0034-8910-rsp-48-2", "id": "0034-8910-rsp-48-2"}
         self.DBCollectionMock.find_one.return_value = manifest
-        bundles = adapters.DocumentsBundleStore(self.DBCollectionMock)
-        bundle = bundles.fetch("0034-8910-rsp-48-2")
+        store = self.Adapter(self.DBCollectionMock)
+        data = store.fetch("0034-8910-rsp-48-2")
         # XXX: Teste incompleto, pois não testa o retorno de forma precisa
-        self.assertEqual(bundle.id(), "0034-8910-rsp-48-2")
-        self.assertEqual(bundle.manifest, manifest)
+        self.assertEqual(data.id(), "0034-8910-rsp-48-2")
+        self.assertEqual(data.manifest, manifest)
 
     def test_update(self):
-        bundles = adapters.DocumentsBundleStore(self.DBCollectionMock)
-        bundle = domain.DocumentsBundle(id="0034-8910-rsp-48-2")
-        bundles.update(bundle)
-        expected = bundle.manifest
+        store = self.Adapter(self.DBCollectionMock)
+        data = self.DomainClass(id="0034-8910-rsp-48-2")
+        store.update(data)
+        expected = data.manifest
         expected["_id"] = "0034-8910-rsp-48-2"
         self.DBCollectionMock.replace_one.assert_called_once_with(
             {"_id": "0034-8910-rsp-48-2"}, expected
@@ -72,16 +70,20 @@ class DocumentsBundleStoreTest(unittest.TestCase):
 
     def test_update_raises_exception_if_does_not_exist(self):
         self.DBCollectionMock.replace_one.return_value = Mock(matched_count=0)
-        bundles = adapters.DocumentsBundleStore(self.DBCollectionMock)
-        bundle = domain.DocumentsBundle(id="0034-8910-rsp-48-2")
-        self.assertRaises(exceptions.DoesNotExist, bundles.update, bundle)
+        store = self.Adapter(self.DBCollectionMock)
+        data = self.DomainClass(id="0034-8910-rsp-48-2")
+        self.assertRaises(exceptions.DoesNotExist, store.update, data)
 
     def test_update_with_manifest_without__id(self):
-        bundles = adapters.DocumentsBundleStore(self.DBCollectionMock)
-        bundle = domain.DocumentsBundle(
-            manifest={"_id": "1", "id": "0034-8910-rsp-48-2"}
-        )
-        bundles.update(bundle)
+        store = self.Adapter(self.DBCollectionMock)
+        data = self.DomainClass(manifest={"_id": "1", "id": "0034-8910-rsp-48-2"})
+        store.update(data)
         self.DBCollectionMock.replace_one.assert_called_once_with(
-            {"_id": "1"}, bundle.manifest
+            {"_id": "1"}, data.manifest
         )
+
+
+class DocumentsBundleStoreTest(StoreTestMixin, unittest.TestCase):
+
+    Adapter = adapters.DocumentsBundleStore
+    DomainClass = domain.DocumentsBundle
